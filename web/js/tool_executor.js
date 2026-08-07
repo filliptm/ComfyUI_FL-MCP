2e44a24f0d2535848c3319a19e13310c5f2db8dd
mi-lxo <milan@liquidxo.com>
2026-08-07 20:06:56 +0200

    Use uniform workflow reveal pacing

--- web/js/tool_executor.js
+++ web/js/tool_executor.js
@@ -15,8 +15,8 @@ import {
 } from "./workflow_plan_apply.js";
 
 const WORKFLOW_REVEAL_DELAYS_MS = Object.freeze({
-    node: 900,
-    connection: 600,
+    node: 700,
+    connection: 700,
 });
 
 /**
